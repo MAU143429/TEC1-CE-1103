53371c9996c1be02582cb0a6012dd50ec0406fd6
MAU143429 <48070743+MAU143429@users.noreply.github.com>
2020-03-04 22:48:04 -0600

    Add files via upload
    Controlador de la interfaz del servidor para el boton de inicio de servidor y establecer el puerto

--- ControllerA.java
+++ ControllerA.java
@@ -4,6 +4,11 @@ package Server;
 import javafx.scene.control.*;
 import javafx.event.ActionEvent;
 
+/**
+ * in this class i control a Server GUI
+ * The action PressButton enable a parameter to established a port, the info from Text field is convert to int and then used to
+ * created a server in this port
+ */
 
 public class ControllerA {
     public TextArea port;
