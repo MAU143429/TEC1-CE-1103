4ec94064aad2bb6602018cf3537c04bbd1c2c13a
MAU143429 <48070743+MAU143429@users.noreply.github.com>
2020-03-04 22:48:27 -0600

    Add files via upload
    Main del servidor full

--- MainA.java
+++ MainA.java
@@ -7,6 +7,10 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+/**
+ * this class start server connection and enable one option to select what server port do you prefer and server runs in this port
+ * one more time apply inheritance  using extends method and i included parameters of GUI
+ */
 public class MainA extends Application  {
 
     @Override
