552998b28de8afc2489e5d139857a2a3cd8999e9
MAU143429 <48070743+MAU143429@users.noreply.github.com>
2020-03-04 22:46:15 -0600

    Add files via upload
    controller de la GUI del Cliente

--- Controller.java
+++ Controller.java
@@ -1,31 +1,33 @@
 package sample;
 
-import Server.Server;
+
 import javafx.event.ActionEvent;
 import javafx.scene.control.*;
+/**
+ * in this class i control a Client GUI
+ * The action PressButton enable a parameter to send messages, the info from Text field is convert to variable and then send info with help of sockets
+ *
+ * i dont apply encapsulated variable because i need this parameter on another class
+ */
 
-public class Controller {
-    public TextArea chat;
-    public TextArea sendbox;
-    public TextArea port14;
-
-
-
-    public void pressButton(ActionEvent event){
-        String text = sendbox.getText();
-        sendbox.clear();
-        chat.appendText("USER 1:" + text+ "\n");
-
-    }
+public class Controller  {
 
 
-    public void pressButton3(ActionEvent event){
-        String port = port14.getText();
-        int port5 = Integer.parseInt(port);
-        Client2 client1 = new Client2(port5);
-        client1.start();
+        public TextArea chat;
+        public TextArea sendbox;
+        public TextArea port14;
+
+
+        public void pressButton (ActionEvent event){
+            String text = sendbox.getText();
+            sendbox.clear();
+            chat.appendText("USER 1:" + text + "\n");
+
+        }
 
 
 
-    }
-    }
+
+
+        }
+
